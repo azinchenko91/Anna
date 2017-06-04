414f268ed2f7f74f54d0c47871c069573e6b1b17
azinchenko91 <azinchenko1991@gmail.com>
2017-06-04 21:23:12 +0300

    Initial commit

---
+++ src/Task1_1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * Created by user on 04.06.2017.
+ */
+public class Task1_1 {
+        public static void main(String[] args) throws Exception {
+            Scanner k = new Scanner(System.in);
+            System.out.println("Enter your name: ");
+            String name = k.nextLine();
+            System.out.print("Hi " + name + "!");
+        }
+    }
+
